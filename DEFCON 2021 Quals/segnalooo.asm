551de449ffb9e61614075494d6894351bdbcf025
Kirin <kirin@KirindeMacBook-Pro.local>
2021-05-04 23:36:04 +0800

    Kirin

--- DEFCON 2021 Quals/segnalooo.asm
+++ DEFCON 2021 Quals/segnalooo.asm
@@ -1,3 +1,3 @@
 Use "\xF1: icebp" bypass signal_handler
 Use syscall in handler bypass seccomp sandbox
-# 感觉把队友坑了，最开始拿到题目自己造的爆破轮子，因为没有控制好阻塞判断，跑出 "\xF1" 后主动 Ctrl + C 了，白白浪费了大片时间
+# 感觉把队友坑了，最开始拿到题目自己造的爆破轮子，因为没有控制好阻塞判断，跑出 "\xF1" 后自己手动 Ctrl + C 了，白白浪费了大片时间
